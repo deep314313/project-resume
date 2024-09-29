8222ea79046abd295d0c2410c819827a1189598a
deep314313 <123625000+deep314313@users.noreply.github.com>
2024-09-30 00:59:49 +0530

    Update page.tsx

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -50,7 +50,7 @@ function billing() {
     const options={
       "key":process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
       "subscription_id":subId,
-      "name":'Tubeguruji AI Apps',
+      "name":'Deepanshu Gupta',
       description:'Monthly Subscription',
       handler:async(resp:any)=>{
         console.log(resp);
